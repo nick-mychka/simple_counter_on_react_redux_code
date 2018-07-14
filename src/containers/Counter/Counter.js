085bf1699aa9f9c53413900c31b1dac17a964aaf
Nick Mychka <nickmychka@gmail.com>
2018-07-14 14:08:43 +0300

    improved redux code

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import * as actionCreators from '../../store/actions/index';
 
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import Button from '../../components/Button/Button';
-import * as actionTypes from '../../store/action';
 
 class Counter extends Component {
 	// state = {
@@ -62,12 +62,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-		onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-		onAddCounter: () => dispatch({type: actionTypes.ADD, value: 5}),
-		onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 5}),
-		onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-		onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
+		onIncrementCounter: () => dispatch(actionCreators.increment()),
+		onDecrementCounter: () => dispatch(actionCreators.decrement()),
+		onAddCounter: () => dispatch(actionCreators.add(5)),
+		onSubtractCounter: () => dispatch(actionCreators.subtract(5)),
+		onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+		onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
 	};
 } ;
 
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,26 +1,24 @@
-import * as actionTypes from '../action';
+import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
 	results: []
 }
+
+const deleteResult = (state, action) => {
+	const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+	return updateObject(state, {results: updatedArray});
+}
+
 const reducer = (state = initialState, action) => {
 	switch(action.type) {
 		case actionTypes.STORE_RESULT:
-			return {
-				...state,
-				results: state.results.concat({id: new Date(), value: action.result})
-			}
+			return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
 		case actionTypes.DELETE_RESULT:
-			// const id = 2;
-			// const NewArray = [...state.results];
-			// NewArray.slice(id, 1);
-			const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-			return {
-				...state,
-				results: updatedArray
-			}
+			return deleteResult(state, action);		
+		default:
+			return state;
 	}
-	return state;
 }
 
 export default reducer;
